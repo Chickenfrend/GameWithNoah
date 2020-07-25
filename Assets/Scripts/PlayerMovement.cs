d1432e4c5c413b62291136320375a3be77bba949
Chickenfrend <chickenfrend@gmail.com>
2020-07-25 16:02:44 -0700

    Trying to get the player to work (wip)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float jumpHeight = 6.5f;
+    public float gravityScale = 1.0f;
+
+    public CharacterController controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
