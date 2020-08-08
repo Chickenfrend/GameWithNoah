cc7a5ad498ab6aad7cd6341d3d2d68c9a9d726ec
Chickenfrend <chickenfrend@gmail.com>
2020-08-08 13:44:28 -0700

    In the process of redoing player controls
    In the process of redoing player controls

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    BoxCollider2D collider;
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+}
